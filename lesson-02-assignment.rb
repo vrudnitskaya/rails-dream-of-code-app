119874c720255cf64e7f73a419223dff1caae010
Valentina <vrudnitskaya@gmail.com>
2025-03-23 13:42:28 -0500

    question 2

--- lesson-02-assignment.rb
+++ lesson-02-assignment.rb
@@ -7,3 +7,21 @@ spring2026 = Trimester.create(year: '2026', term: 'Spring', application_deadline
 CodingClass.all.each do |coding_class|
     Course.create(coding_class_id: coding_class.id, trimester_id: spring2026.id, max_enrollment: 25)
 end
+
+# Question 2
+# Create a new student record and enroll the student in the Intro to Programming course for the Spring 2026 trimester.
+new_student = Student.create(first_name: 'Harry', last_name: 'Potter', email: 'harrypotter@owl.post')
+trimester = Trimester.find_by(year: '2026', term: 'Spring') 
+conding_class=CodingClass.find_by(title: 'Intro to Programming')
+course=Course.find_by(coding_class_id: coding_class.id, trimester_id: trimester.id)
+enrollment=Enrollment.create(course_id: course.id, student_id: new_student.id)
+
+# Find a mentor with no more than 2 students (enrollments) assigned and assign that mentor to your new student's enrollment.
+mentor=MentorEnrollmentAssignment
+  .group(:mentor_id)
+  .having('COUNT(mentor_id)<=2')
+  .limit(1)
+  .pluck(:mentor_id)
+if mentor.present?
+    MentorEnrollmentAssignment.create(mentor_id: mentor.first, enrollment_id: enrollment.id)
+end
